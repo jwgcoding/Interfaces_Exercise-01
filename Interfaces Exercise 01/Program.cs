6507ffb6ef1b66cd264d6e3b1c6d80c1471d474b
johndward01 <johndward01@gmail.com>
2021-08-16 16:55:04 -0500

    Add project files.

---
+++ Interfaces Exercise 01/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interfaces_Exericse_01_Answers
+{
+    class Program
+    {
+        // TODO: Create an Interface called IParts
+        // TODO: Create a BuildPart() stubbed out method that has a return type of IParts       
+
+        // TODO: Create a class called PlanePart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called TruckPart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called CarPart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called BikePart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+        // TODO: Create a class called MiscPart that conforms to IParts
+        // TODO: Implement the interface and creatively let the user know when a part is being built and when that part is done
+
+
+        // BONUS
+        // TODO: Create methods to abstract away most of the code from your Main() method
+        // TODO: Create a Utility class to encapsulate those methods into 1 location
+        // TODO: Create a folder to hold all of your Part Classes in 1 location
+
+        static void Main(string[] args)
+        {
+            // TODO: Create an IParts List called partsList that will hold all of the parts we want to create
+
+
+            // TODO: Create a do-while loop that only ends when the user types "no" (or 'n')
+
+            // TODO: Greet the user and ask them which part they would like to create
+
+
+            // TODO: Call that parts BuildPart() method
+
+
+            // TODO: Add the part to the partsList
+
+
+            // TODO: Ask them if they would like to continue or exit the program
+
+
+            // TODO: Print out all the parts in the parts list to the console
+
+        }
+    }
+}
